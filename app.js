5627e8460d60d631e87cdcf791bc33c5793e9b14
MikeShahin <gobias101@gmail.com>
2022-03-31 12:01:42 -0700

    filled out get request

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=racist&type=single";
 let joke = document.getElementById("joke");
 
 function getJoke() {
-    
+    fetch(url) 
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        console.log(data.joke)
+    });       
 }
+
+getJoke();
